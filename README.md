f2bc32d5c2c999ef6e1e894706d046f9c3ea36d5
Tech-A56 <tech-a56@Tech-A56s-MacBook-Air.local>
2018-05-02 01:29:56 +0100

    MVP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sparta-Global-TapDefense
+##Instructions
+The player will have to click on the monsters to defeat them before they reach the wall at the bottom of the game.
+The player has 3 lives and the game is played till the player fails. The aim of the game is to see how far the player can reach and this is tracked by a score which is at the bottom of the screen
+The monster  movement speed was designed for a laptop track pad which means using a mouse will most likely be easier.
--- index.js
+++ index.js
@@ -1,185 +1,143 @@
-var i=0;
-var spawn=0;
-
-var lives=3;
-var elem=[];
+//handles the player lives
+function livesFunction(){
+  var lives=3;
+  return lives;
+}
+//global variables
+var hidden, visibilityChange,enemySpawningVar,enemyMovingVar,enemyMoveInterval;
+var life=livesFunction();
+var enemyArray=[];
 var score=0;
 var game=document.getElementById("game");
 var menu=document.getElementById("Menu");
-var button= document.getElementById('StartButton');
+var StartButton= document.getElementById('StartButton');
 var retryButton= document.getElementById('retry');
 var gameOverUI=document.getElementById("gameover");
 var retryButtonContainer=document.getElementById("ButtonContainer");
 var scoreDiv=document.getElementById("score");
-var d;
-var a;
-//var funcClone=myMove();
-var spawned=false;
-var spawnSpeed=400;
-var speed =15;
-
-
-
-//handles the spawning
-function spawning(){
-
-  spawn++;
-  spawned=true;
-  //console.log(spawned);
-  //console.log(elem);
-  // creates a div elements and saves it in a variable
-  var divr = document.createElement("div");
-
-  //attaches the divr variable to the game id elements in the index.html
-  //document.getElementById("game").appendChild(divr);
-  //adds a class to the divr elemetns so it can be styled by the index.css
-  divr.classList.add('SpawnPoint');
-  //adds the element to the array
-  elem.push(divr);
-  //randomly generates a number which is the deciding factor for where eneimes will be spawned
-  var randomNumber =Math.floor((Math.random() * 10) + 1);
-  //the if statement decides where the enimes will be spawned
-
-  //test
-  myMove(divr);
-  //test
-  if(randomNumber==1){
-    //divr.style.left="30px";
-    document.getElementById("1").appendChild(divr);
-  }
-  else if (randomNumber==2) {
-    document.getElementById("2").appendChild(divr);
-    //divr.style.left="60px";
-  }
-  else if (randomNumber==3) {
-    document.getElementById("3").appendChild(divr);
-    //divr.style.left="90px";
-  }
-  else if (randomNumber==4) {
-    document.getElementById("4").appendChild(divr);
-    //divr.style.left="120px";
-  }
-
-  else if (randomNumber==5) {
-    document.getElementById("5").appendChild(divr);
-  }
-  else if (randomNumber==6) {
-    document.getElementById("6").appendChild(divr);
-  }
-  else if (randomNumber==7) {
-    document.getElementById("7").appendChild(divr);
-  }
-  else if (randomNumber==8) {
-    document.getElementById("8").appendChild(divr);
-  }
-  else if (randomNumber==9) {
-    document.getElementById("9").appendChild(divr);
-  }else {
-    document.getElementById("10").appendChild(divr);
-  }
-  a = setInterval(myMove,spawnSpeed);
-  //stop the spawning of the enemies when it has reached its cap
-  if(spawn==10){
-    clearInterval(d);
-  }
-  for (var i = 0; i < elem.length; i++) {
-    var button=elem[i];
-    //buttons[i].value=button.value;
-  }
-    button.addEventListener('click',function(event){
-      console.log("hit");
-      //clearInterval(id);
-      this.remove();
-      //myMove();
-      //this.innerHTML="H";
-      //this.remove();
-      score++;
-      scoreDiv.innerHTML="score: "+score;
-      console.log(score);
-    });
-
+var LivesDiv=document.getElementById("lives");
+var spawnSpeed=500;
+var movementSpeed=5;
+//constally checks to see if the page is active
+setInterval(handleVisibilityChange,1000);
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
 }
-
-
-var id;
-var destroyed =false;
-
-//handles the movement
-
-
-function myMove(enemy) {
-
+//when the page is hidden it stop the enemy spawning and moving
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    clearInterval(enemySpawningVar);
+    clearInterval(enemyMovingVar);
+  }
+}
+//handles the enemy movement
+function enemyMoving(enemy) {
   if(enemy!=null){
-    destroyed=false;
-
-    var pos = 0;
-    id = setInterval(frame, speed);
-    function frame() {
-
-
+    var enemyPosition = 0;
+    enemyMoveInterval = setInterval(enemyMoveHeight,movementSpeed );
+    function enemyMoveHeight() {
       if(enemy.offsetTop>=630){
-        //clearInterval(id);
-        lives--;
-
+        life--;
+        LivesDiv.innerHTML="lives "+life;
         enemy.remove();
-        console.log(lives);
-
-        if(lives<=0){
+        if(life<=0){
           gameOver();
-
-
+          return life=3;
         }
-
       }
-
       else if(enemy.offsetTop<=629){
-        pos++;
-        enemy.style.top = pos + 'px';
-        //enemy.style.backgroundColor="black";
-        //console.log(pos);
+        enemyPosition++;
+        enemy.style.top = enemyPosition + 'px';
       }
     }
   }
 }
-
-
-
-
-
-
-function gameOver(){
-  //gameOverUI.style.display="block";
-  console.log("Game over");
-  ButtonContainer.style.display="block";
-  clearInterval(d);
-  clearInterval(a);
-  for (var i = 0; i <elem.length; i++) {
-    elem[i].remove();
-  }
-}
-
-
-
-
-button.addEventListener('click',function(event){
+//the start button whihc will hide the menu and start the game
+StartButton.addEventListener('click',function(event){
   game.style.display="block";
   menu.style.display="none";
-  console.log("button hit");
-  d = setInterval(spawning,spawnSpeed);
-
+  enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
 });
-
+//the restart button which reset the game and hide the button
 retry.addEventListener('click',function(event){
-  console.log("retry button pressed")
   restart();
 });
+//handles the spawning
+function enemySpawning(){
+  // creates a div elements and saves it in a variable
+  var enemyDiv = document.createElement("div");
+  //adds a class to the enemydiv elemetns so it can be styled by the index.css
+  enemyDiv.classList.add('enemy');
+  //adds the enemydiv to the array
+  enemyArray.push(enemyDiv);
+  //randomly generates a number which is the deciding factor for where eneimes will be spawned
+  var randomNumber =Math.floor((Math.random() * 10) + 1);
+  //the if statement decides where the enimes will be spawned
+  enemyMoving(enemyDiv);
+  if(randomNumber==1){
+    document.getElementById("1").appendChild(enemyDiv);
+  }
+  else if (randomNumber==2) {
+    document.getElementById("2").appendChild(enemyDiv);
+  }
+  else if (randomNumber==3) {
+    document.getElementById("3").appendChild(enemyDiv);
+  }
+  else if (randomNumber==4) {
+    document.getElementById("4").appendChild(enemyDiv);
+  }
+  else if (randomNumber==5) {
+    document.getElementById("5").appendChild(enemyDiv);
+  }
+  else if (randomNumber==6) {
+    document.getElementById("6").appendChild(enemyDiv);
+  }
+  else if (randomNumber==7) {
+    document.getElementById("7").appendChild(enemyDiv);
+  }
+  else if (randomNumber==8) {
+    document.getElementById("8").appendChild(enemyDiv);
+  }
+  else if (randomNumber==9) {
+    document.getElementById("9").appendChild(enemyDiv);
+  }else {
+    document.getElementById("10").appendChild(enemyDiv);
+  }
+  enemyMovingVar = setInterval(enemyMoving,movementSpeed);
+  //help with applying an event listener to the enemies
+  for (var i = 0; i < enemyArray.length; i++) {
+    var enemy=enemyArray[i];
+  }
+  //when the enemy is cliked it displays the new score and destorys the enemy
+  enemy.addEventListener('click',function(event){
+    this.remove();
+    score++;
+    scoreDiv.innerHTML="score: "+score;
 
+  });
+}
+//function that stops the game and brings up the retry button
+function gameOver(){
+  ButtonContainer.style.display="block";
+  clearInterval(enemySpawningVar);
+  clearInterval(enemyMovingVar);
+  for (var i = 0; i <enemyArray.length; i++) {
+    enemyArray[i].remove();
+  }
+}
+//function that resets the value and elements
 function restart(){
-lives=3;
-elem=[];
-score=0;
-spawn=0;
-d = setInterval(spawning,spawnSpeed);
-ButtonContainer.style.display="none";
-scoreDiv.innerHTML="score: "+score;
+  enemyArray=[];
+  score=0;
+  spawn=0;
+  enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
+  ButtonContainer.style.display="none";
+  scoreDiv.innerHTML="score: "+score;
 }
