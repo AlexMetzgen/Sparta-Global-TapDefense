7ecf0b3f1619cd4f73bb51d9c1b40f72e4da14c0
Tech-A56 <tech-a56@Tech-A56s-MacBook-Air.local>
2018-05-03 20:41:44 +0100

    final game

--- index.js
+++ index.js
@@ -1,133 +1,163 @@
-//handles the player lives
-function livesFunction(){
-  var lives=3;
-  return lives;
-}
 //global variables
-var hidden, visibilityChange,enemySpawningVar,enemyMovingVar,enemyMoveInterval;
-var life=livesFunction();
+var spawnSpeed=600;
+var movementSpeed=20;
+var lives=0;
 var enemyArray=[];
 var score=0;
-var game=document.getElementById("game");
-var menu=document.getElementById("Menu");
-var StartButton= document.getElementById('StartButton');
-var retryButton= document.getElementById('retry');
-var gameOverUI=document.getElementById("gameover");
-var retryButtonContainer=document.getElementById("ButtonContainer");
+var hidden,enemySpawningVar,enemyMovingVar,enemyMoveInterval;
 var scoreDiv=document.getElementById("score");
 var LivesDiv=document.getElementById("lives");
-var spawnSpeed=500;
-var movementSpeed=5;
-//constally checks to see if the page is active
-setInterval(handleVisibilityChange,1000);
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
+var isHidden=false;
+var start=false;
+backgroundMusic();
+isItHidden();
+//keeps calling the function every 100 miliseconds
+setInterval(handleVisibilityChange,100);
+//used to check is the game is hidden or not
+function isItHidden(){
+  var visibilityChange;
+  if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+  } else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+  } else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+  }
 }
 //when the page is hidden it stop the enemy spawning and moving
 function handleVisibilityChange() {
-  if (document[hidden]) {
-    clearInterval(enemySpawningVar);
-    clearInterval(enemyMovingVar);
+  if (document[hidden]==true) {
+    if(isHidden==false){
+      clearInterval(enemySpawningVar);
+      clearInterval(enemyMovingVar);
+      //clearInterval(enemyMoveInterval);
+      if(start==true){
+        isHidden=true;
+      }
+    }
   }
+  //when the playey comes back to the game the enemies start mobing and spawning again
+  else if (document[hidden]==false){
+    if(isHidden==true&&lives>0&&start==true){
+      enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
+      //enemyMoveInterval = setInterval(enemyMoving,movementSpeed);
+      isHidden=false;
+    }
+  }
+}
+//handles the spawning
+function enemySpawning(){
+  // creates a div elements and saves it in a variable
+  var enemyDiv = document.createElement("div");
+  //adds a class to the enemydiv element so it can be styled by the index.css
+  enemyDiv.classList.add('enemy');
+  //adds the enemydiv to the array
+  enemyArray.push(enemyDiv);
+  //randomly generates a number which is the deciding factor for where eneimes will be spawned
+  var randomNumber =Math.floor((Math.random() * 10) + 1);
+  //calls the enemyMoving function and passes it the created enemy div element
+  enemyMoving(enemyDiv);
+  //the switch statement decides where the enemies will be spawned
+  switch (randomNumber) {
+    case 1:
+    document.getElementById("1").appendChild(enemyDiv);
+    break;
+    case 2:
+    document.getElementById("2").appendChild(enemyDiv);
+    break;
+    case 3:
+    document.getElementById("3").appendChild(enemyDiv);
+    break;
+    case 4:
+    document.getElementById("4").appendChild(enemyDiv);
+    break;
+    case 5:
+    document.getElementById("5").appendChild(enemyDiv);
+    break;
+    case 6:
+    document.getElementById("6").appendChild(enemyDiv);
+    break;
+    case 7:
+    document.getElementById("7").appendChild(enemyDiv);
+    break;
+    case 8:
+    document.getElementById("8").appendChild(enemyDiv);
+    break;
+    case 9:
+    document.getElementById("9").appendChild(enemyDiv);
+    break;
+    default:document.getElementById("10").appendChild(enemyDiv);
+  }
+  //helps with applying an event listener to the enemies
+  for (var i = 0; i < enemyArray.length; i++) {
+    var enemy=enemyArray[i];
+  }
+  //when the enemy is cliked it displays the new score and destroys the enemy
+  enemy.addEventListener('click',function(event){
+    this.remove();
+    score++;
+    scoreDiv.innerHTML="score: "+score;
+    movementSpeed-=0.5;
+    var hitAudio = new Audio('hit.wav');
+    hitAudio.play();
+  });
 }
 //handles the enemy movement
 function enemyMoving(enemy) {
   if(enemy!=null){
     var enemyPosition = 0;
+    //the function is always running to see if the enenmy has reach a certain height
+    //
     enemyMoveInterval = setInterval(enemyMoveHeight,movementSpeed );
+    //enemyMoveheight handles when when the enemies reaches the certain distance the player loses a life, a sound is played and the enemy is destroyed
     function enemyMoveHeight() {
-      if(enemy.offsetTop>=630){
-        life--;
-        LivesDiv.innerHTML="lives "+life;
+      if(isHidden==true){return;}
+      if(enemy.offsetTop>=700){
+        lives--;
+        var audioLifeLost= new Audio("lifeLost.wav");
+        audioLifeLost.play();
+        LivesDiv.innerHTML="lives "+lives;
         enemy.remove();
-        if(life<=0){
+        //when the play reaches 0 lives the function is called to end the game
+        if(lives<=0){
           gameOver();
-          return life=3;
         }
       }
-      else if(enemy.offsetTop<=629){
+      //enemy will keep moving until it has reacher 669px
+      else if(enemy.offsetTop<=699){
         enemyPosition++;
         enemy.style.top = enemyPosition + 'px';
       }
     }
   }
 }
-//the start button whihc will hide the menu and start the game
+//the start button which will hide the menu, start the game and display the lives and score
 StartButton.addEventListener('click',function(event){
+  var menu=document.getElementById("Menu");
+  var StartButton= document.getElementById('StartButton');
+  var game=document.getElementById("game");
   game.style.display="block";
   menu.style.display="none";
   enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
+  scoreDiv.innerHTML="score: "+score;
+  LivesDiv.innerHTML="lives: 3";
+  lives=3;
+  start=true;
 });
-//the restart button which reset the game and hide the button
+//the retry button reset the game and hide the button
 retry.addEventListener('click',function(event){
   restart();
 });
-//handles the spawning
-function enemySpawning(){
-  // creates a div elements and saves it in a variable
-  var enemyDiv = document.createElement("div");
-  //adds a class to the enemydiv elemetns so it can be styled by the index.css
-  enemyDiv.classList.add('enemy');
-  //adds the enemydiv to the array
-  enemyArray.push(enemyDiv);
-  //randomly generates a number which is the deciding factor for where eneimes will be spawned
-  var randomNumber =Math.floor((Math.random() * 10) + 1);
-  //the if statement decides where the enimes will be spawned
-  enemyMoving(enemyDiv);
-  if(randomNumber==1){
-    document.getElementById("1").appendChild(enemyDiv);
-  }
-  else if (randomNumber==2) {
-    document.getElementById("2").appendChild(enemyDiv);
-  }
-  else if (randomNumber==3) {
-    document.getElementById("3").appendChild(enemyDiv);
-  }
-  else if (randomNumber==4) {
-    document.getElementById("4").appendChild(enemyDiv);
-  }
-  else if (randomNumber==5) {
-    document.getElementById("5").appendChild(enemyDiv);
-  }
-  else if (randomNumber==6) {
-    document.getElementById("6").appendChild(enemyDiv);
-  }
-  else if (randomNumber==7) {
-    document.getElementById("7").appendChild(enemyDiv);
-  }
-  else if (randomNumber==8) {
-    document.getElementById("8").appendChild(enemyDiv);
-  }
-  else if (randomNumber==9) {
-    document.getElementById("9").appendChild(enemyDiv);
-  }else {
-    document.getElementById("10").appendChild(enemyDiv);
-  }
-  enemyMovingVar = setInterval(enemyMoving,movementSpeed);
-  //help with applying an event listener to the enemies
-  for (var i = 0; i < enemyArray.length; i++) {
-    var enemy=enemyArray[i];
-  }
-  //when the enemy is cliked it displays the new score and destorys the enemy
-  enemy.addEventListener('click',function(event){
-    this.remove();
-    score++;
-    scoreDiv.innerHTML="score: "+score;
-
-  });
-}
 //function that stops the game and brings up the retry button
 function gameOver(){
   ButtonContainer.style.display="block";
   clearInterval(enemySpawningVar);
   clearInterval(enemyMovingVar);
+  var deadAudio = new Audio('dead.wav');
+  deadAudio.play();
   for (var i = 0; i <enemyArray.length; i++) {
     enemyArray[i].remove();
   }
@@ -137,7 +167,16 @@ function restart(){
   enemyArray=[];
   score=0;
   spawn=0;
-  enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
   ButtonContainer.style.display="none";
   scoreDiv.innerHTML="score: "+score;
+  LivesDiv.innerHTML="lives: 3";
+  enemySpawningVar = setInterval(enemySpawning,spawnSpeed);
+  lives=3;
+  movementSpeed=20;
+}
+//function that holds the background music
+function backgroundMusic(){
+  var backgroundAudio = new Audio('8bit-music.ogg');
+  backgroundAudio.play();
+  backgroundAudio.loop=true;
 }
